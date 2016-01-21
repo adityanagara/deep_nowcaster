7f88f173bda87aeb95e0b55ef872880e4709e741
aditya <adinagarajan1@gmail.com>
2016-01-21 09:13:19 -0500

    runnable models

--- code/DCNN_nowcaster.py
+++ code/DCNN_nowcaster.py
@@ -162,7 +162,7 @@ def build_DCNN_2(input_var = None):
     l_in = lasagne.layers.InputLayer(shape=(None, 8, 33, 33),
                                         input_var=input_var)
     
-    l_conv1 = dnn.Conv2DLayer(
+    l_conv1 = dnn.Conv2DDNNLayer(
             l_in,
             num_filters=32,
             filter_size=(11, 11),
@@ -172,7 +172,7 @@ def build_DCNN_2(input_var = None):
             b=lasagne.init.Constant(.1)
         )
     
-    l_conv2 = dnn.Conv2DLayer(
+    l_conv2 = dnn.Conv2DDNNLayer(
             l_conv1,
             num_filters=48,
             filter_size=(4, 4),
@@ -216,7 +216,8 @@ def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
 
                                    
 
-def main(num_epochs = 1000,num_points = 10,compute_flag='cpu'):
+def main(num_epochs = 100,num_points = 10,compute_flag='cpu'):
+    num_epochs = int(num_epochs)
     results_file_name = 'exp_' + str(num_epochs) + '_' + str(num_points) + '_' + compute_flag + '.csv'
     network_file_name = 'network_' + str(num_epochs) + '_' + str(num_points) + '_' + compute_flag 
     num_points = int(num_points)
@@ -230,13 +231,14 @@ def main(num_epochs = 1000,num_points = 10,compute_flag='cpu'):
     data_builder = BuildDataSet.dataset(Threshold = None)
     # Sample 1500 points and make the IPW and refl frames
     PixelPoints = data_builder.sample_random_pixels()
+    print('Building training set...')
     train_set = data_builder.make_points_frames(PixelPoints[:num_points])
     X_train,Y_train = data_builder.arrange_frames(train_set)
     # Define the input tensor
     input_var = T.tensor4('inputs')
     # Define the output tensor (in this case it is a real value or reflectivity values)
     output_var = T.fcol('targets')
-    
+    print('Building validation set...')
     # Build the validation set take the last num_points
     validation_set = data_builder.make_points_frames(PixelPoints[-num_points:])
     X_val,Y_val = data_builder.arrange_frames(validation_set)
@@ -248,6 +250,7 @@ def main(num_epochs = 1000,num_points = 10,compute_flag='cpu'):
     elif compute_flag == 'cpu2':
         network,l_hidden1 = build_CNN_2(input_var)
     elif compute_flag == 'gpu2':
+        print('gpu2 experiment')
         network,l_hidden1 = build_DCNN_2(input_var)
     else:
         network,l_hidden1 = build_DCNN(input_var)
@@ -273,7 +276,7 @@ def main(num_epochs = 1000,num_points = 10,compute_flag='cpu'):
     # Define theano function which generates and compiles C code for the optimization problem
     train_fn = theano.function([input_var, output_var], loss, updates=updates)
     
-    test_fn = theano.function([input_var, output_var],test_loss)
+    test_fn = theano.function([input_var, output_var],test_loss, updates=updates)
     
     experiment_start_time = time.time()
     # Start training
@@ -284,7 +287,6 @@ def main(num_epochs = 1000,num_points = 10,compute_flag='cpu'):
         start_time = time.time()
         for batch in iterate_minibatches(X_train, Y_train, 1059, shuffle=False):
             inputs, targets = batch
-            print inputs.shape,targets.shape
             train_err += train_fn(inputs, targets)
             train_batches += 1
         # And a full pass over the validation data:
@@ -292,7 +294,7 @@ def main(num_epochs = 1000,num_points = 10,compute_flag='cpu'):
         val_batches = 0
         for batch in iterate_minibatches(X_val, Y_val, 1059, shuffle=False):
             inputs, targets = batch
-            err, acc = test_fn(inputs, targets)
+            err = test_fn(inputs, targets)
             val_err += err
             val_batches += 1
         
@@ -303,7 +305,8 @@ def main(num_epochs = 1000,num_points = 10,compute_flag='cpu'):
         append_file(results_file_name,epoch + 1,round(train_err / train_batches,2),val_err / val_batches)
         
         # Dump the network file every 100 epochs
-        if epoch + 1 % 100 == 0:
+        if (epoch + 1) % 100 == 0:
+            print('creating network file')
             network_file = file('output/'+ network_file_name + '_' + str(epoch + 1) + '.pkl','wb')
             cPickle.dump(network,network_file,protocol = cPickle.HIGHEST_PROTOCOL)
             network_file.close()
@@ -327,106 +330,6 @@ if __name__ == '__main__':
     
     print 'Done!' 
 
-#def main(model='mlp', num_epochs=500):
-#    # Load the dataset
-#    print("Loading data...")
-#    X_train, y_train, X_val, y_val, X_test, y_test = load_dataset()
-#
-#    # Prepare Theano variables for inputs and targets
-#    input_var = T.tensor4('inputs')
-#    target_var = T.ivector('targets')
-#
-#    # Create neural network model (depending on first command line parameter)
-#    print("Building model and compiling functions...")
-#    if model == 'mlp':
-#        network = build_mlp(input_var)
-#    elif model.startswith('custom_mlp:'):
-#        depth, width, drop_in, drop_hid = model.split(':', 1)[1].split(',')
-#        network = build_custom_mlp(input_var, int(depth), int(width),
-#                                   float(drop_in), float(drop_hid))
-#    elif model == 'cnn':
-#        network = build_cnn(input_var)
-#    else:
-#        print("Unrecognized model type %r." % model)
-#
-#    # Create a loss expression for training, i.e., a scalar objective we want
-#    # to minimize (for our multi-class problem, it is the cross-entropy loss):
-#    prediction = lasagne.layers.get_output(network)
-#    loss = lasagne.objectives.categorical_crossentropy(prediction, target_var)
-#    loss = loss.mean()
-#    # We could add some weight decay as well here, see lasagne.regularization.
-#
-#    # Create update expressions for training, i.e., how to modify the
-#    # parameters at each training step. Here, we'll use Stochastic Gradient
-#    # Descent (SGD) with Nesterov momentum, but Lasagne offers plenty more.
-#    params = lasagne.layers.get_all_params(network, trainable=True)
-#    updates = lasagne.updates.nesterov_momentum(
-#            loss, params, learning_rate=0.01, momentum=0.9)
-#
-#    # Create a loss expression for validation/testing. The crucial difference
-#    # here is that we do a deterministic forward pass through the network,
-#    # disabling dropout layers.
-#    test_prediction = lasagne.layers.get_output(network, deterministic=True)
-#    test_loss = lasagne.objectives.categorical_crossentropy(test_prediction,
-#                                                            target_var)
-#    test_loss = test_loss.mean()
-#    # As a bonus, also create an expression for the classification accuracy:
-#    test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=1), target_var),
-#                      dtype=theano.config.floatX)
-#
-#    # Compile a function performing a training step on a mini-batch (by giving
-#    # the updates dictionary) and returning the corresponding training loss:
-#    train_fn = theano.function([input_var, target_var], loss, updates=updates)
-#
-#    # Compile a second function computing the validation loss and accuracy:
-#    val_fn = theano.function([input_var, target_var], [test_loss, test_acc])
-#
-#    # Finally, launch the training loop.
-#    print("Starting training...")
-#    # We iterate over epochs:
-#    for epoch in range(num_epochs):
-#        # In each epoch, we do a full pass over the training data:
-#        train_err = 0
-#        train_batches = 0
-#        start_time = time.time()
-#        for batch in iterate_minibatches(X_train, y_train, 500, shuffle=True):
-#            inputs, targets = batch
-#            train_err += train_fn(inputs, targets)
-#            train_batches += 1
-#
-#        # And a full pass over the validation data:
-#        val_err = 0
-#        val_acc = 0
-#        val_batches = 0
-#        for batch in iterate_minibatches(X_val, y_val, 500, shuffle=False):
-#            inputs, targets = batch
-#            err, acc = val_fn(inputs, targets)
-#            val_err += err
-#            val_acc += acc
-#            val_batches += 1
-#
-#        # Then we print the results for this epoch:
-#        print("Epoch {} of {} took {:.3f}s".format(
-#            epoch + 1, num_epochs, time.time() - start_time))
-#        print("  training loss:\t\t{:.6f}".format(train_err / train_batches))
-#        print("  validation loss:\t\t{:.6f}".format(val_err / val_batches))
-#        print("  validation accuracy:\t\t{:.2f} %".format(
-#            val_acc / val_batches * 100))
-#
-#    # After training, we compute and print the test error:
-#    test_err = 0
-#    test_acc = 0
-#    test_batches = 0
-#    for batch in iterate_minibatches(X_test, y_test, 500, shuffle=False):
-#        inputs, targets = batch
-#        err, acc = val_fn(inputs, targets)
-#        test_err += err
-#        test_acc += acc
-#        test_batches += 1
-#    print("Final results:")
-#    print("  test loss:\t\t\t{:.6f}".format(test_err / test_batches))
-#    print("  test accuracy:\t\t{:.2f} %".format(
-#        test_acc / test_batches * 100))
 
     
         
---
+++ code/nowcaster_experiments.sh
@@ -0,0 +1,4 @@
+!#/bin/bash
+python code/DCNN_nowcaster.py 1000 40 gpu2
+python code/DCNN_nowcaster.py 1000 50 gpu2
+python code/DCNN_nowcaster.py 1000 60 gpu2
