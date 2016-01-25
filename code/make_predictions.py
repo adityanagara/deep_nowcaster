aa7b4299115d18360062d7534057adbf09926ab6
aditya <adinagarajan1@gmail.com>
2016-01-25 16:51:15 -0500

    prediction movies updated to nowcast

--- code/make_predictions.py
+++ code/make_predictions.py
@@ -31,12 +31,14 @@ lasagne.layers.set_all_param_values(network, params)
 
 # Build theano function which inturn generates and compiles C code to run the network
 prediction_ = lasagne.layers.get_output(network, deterministic=True)
+
 predict_function = theano.function([input_var], prediction_)
 
 # Load the data builder object
 data_builder = BuildDataSet.dataset(Threshold = None)
 
 # This is the range of points in the central domain which can have a 33x33 around it.
+num_points = 4356
 domain_points = (range(17,83),range(17,83))
 
 # Arrange all pixel points to a list
@@ -57,17 +59,32 @@ PixelPoints = np.array(PixelPoints)
 temp_ipw_file_list = filter(lambda x: x[7:10] in sorted_days['129'],data_builder.IPWfiles)
 temp_radar_file_list = filter(lambda x: x[9:12] in sorted_days['129'],data_builder.Radarfiles)
 
-PixelPoints = PixelPoints[:10]
+#PixelPoints = PixelPoints[:10]
 print PixelPoints.shape
+
+real_predictions = np.zeros((num_points,91,2))
+
+
+
+pt_ctr = 0
 
 for x_,y_ in zip(PixelPoints[:,0],PixelPoints[:,1]):
     print 'Building data set for point: (%d,%d)'%(x_,y_)
     test_set = data_builder.build_features_and_truth(temp_ipw_file_list,temp_radar_file_list,x_,y_)
     
-    X_test,Y_test = data_builder.arrange_frames_single(test_set)
+    X_test,Y_test = data_builder.arrange_frames_single(test_set) 
+    Y_pred = predict_function(X_test)
+    real_predictions[pt_ctr,:,0] = Y_test.reshape(91,)
     
-    print X_test.shape,Y_test.shape
-    
+    real_predictions[pt_ctr,:,1] = Y_pred[0].reshape(91,)
+    pt_ctr+=1
+
+#np.save('test_predictions.npy',real_predictions)
+
+
+
+
+
     
 
 
--- includes/nowcast.py
+++ includes/nowcast.py
@@ -61,7 +61,164 @@ class BuildNowcaster():
         plt.ylim((-150.0,150.0))
         plt.grid()
     
-    def make_prediction(self,doy):
+    def plot_predictions(self,movie_points,save_fige=True):
+        gridX = np.arange(-150.0,151.0,300.0/(100-1))
+        gridY = np.arange(-150.0,151.0,300.0/(100-1))
+        # Define center point of the domain
+        x_ = 49.0
+        y_ = 49.0
+        # Slice the central 66x66 grid
+        x_range_start = gridX[x_] - 33.0*(300.0/99.0)
+        y_range_start = gridY[y_] - 33.0*(300.0/99.0)
+        x_range_end = gridX[x_] + 33.0*(300.0/99.0)
+        y_range_end = gridY[y_] + 33.0*(300.0/99.0)
+        
+        gridX_ = np.arange(x_range_start,x_range_end,300./99.)
+        gridY_ = np.arange(y_range_start,y_range_end,300./99.)
+        
+        time_index = ['{0}{1}'.format(str(x).zfill(2),str(y).zfill(2)) for x in range(24) for y in [0,30]]
+        
+        for x_ in range(91):
+            plt.figure()
+            plt.subplots(1,2)
+            plt.subplot(121)
+            real_ = movie_points[:,:,x_,0]
+            predicted_ = movie_points[:,:,x_,1]
+            real_[real_ < 20.0] = np.nan
+            predicted_[predicted_ < 20.0] = np.nan
+#            gridZ = np.ma.array(gridZ, mask=np.isnan(gridZ))
+            real_ = np.ma.array(real_, mask=np.isnan(real_))
+            predicted_ = np.ma.array(predicted_, mask=np.isnan(predicted_))
+            plt.pcolor(gridX_,gridY_,real_.T,cmap='jet', vmin=0.0, vmax=50.0)
+            plt.xlim((-150.0,150.0))
+            plt.ylim((-150.0,150.0))
+            plt.grid()
+            plt.subplot(122)
+            plt.pcolor(gridX_,gridY_,predicted_.T,cmap='jet', vmin=0.0, vmax=50.0)
+            plt.xlim((-150.0,150.0))
+            plt.ylim((-150.0,150.0))
+            plt.grid()
+    
+            plt.suptitle('Actual and predicted fields for may 9th ' + time_index[x_ - 43] + ' UTC')
+            if save_fige:
+                plt.savefig('output/prediction_movies/Plot_' + str(x_) + '.png')
+
+        
+
+        
+        
+        
+
+
+'''
+raw_data_base_dir = '/Users/adityanagarajan/Summer_2015/ConvectiveInitiation/data/TrainTest/'
+
+
+movie_points = np.load('data/NB_real_predictions.npy')
+
+
+ipw = np.load(raw_data_base_dir + 'IPWdata128_9.npy')
+refl = np.load(raw_data_base_dir + 'RadarRefl128_9.npy')
+
+
+refl[refl < 24.0] = np.nan
+
+refl = np.ma.array(refl, mask=np.isnan(refl))
+
+movie_points = movie_points.reshape(4356,91,2)
+
+print movie_points.shape
+
+movie_points = movie_points.reshape(66,66,91,2)
+
+gridX = np.arange(-150.0,151.0,300.0/(100-1))
+gridY = np.arange(-150.0,151.0,300.0/(100-1))
+
+plt.figure()
+plt.pcolor(gridX,gridY,ipw,cmap='jet', vmin=-3.0, vmax=3.0)
+
+plt.pcolor(gridX,gridY,refl,cmap='jet', vmin=10, vmax=60)
+
+plt.grid()
+plt.xlim((-150.0,150.0))
+
+plt.ylim((-150.0,150.0))
+plt.show()
+
+gridIPW = np.zeros((66,66))
+gridZ = np.zeros((66,66))
+out_point = np.zeros((100,100))
+
+
+gridIPW[:] = np.nan
+gridZ[:] = np.nan
+
+
+
+x_ = 49.0
+y_ = 49.0
+
+x_range_start = gridX[x_] - 33.0*(300.0/99.0)
+y_range_start = gridY[y_] - 33.0*(300.0/99.0)
+
+x_range_end = gridX[x_] + 33.0*(300.0/99.0)
+y_range_end = gridY[y_] + 33.0*(300.0/99.0)
+
+gridX_ = np.arange(x_range_start,x_range_end,300./99.)
+gridY_ = np.arange(y_range_start,y_range_end,300./99.)
+
+plt.figure()
+
+i_start = x_ -33
+i_end = x_ + 33
+j_start = y_ - 33
+j_end = y_ + 33
+
+
+gridIPW[:] = ipw[j_start:j_end,i_start:i_end]
+
+gridZ[:] = refl[j_start:j_end,i_start:i_end]
+
+gridZ = np.ma.array(gridZ, mask=np.isnan(gridZ))
+
+#plt.pcolor(gridX_,gridY_,gridIPW,cmap='jet', vmin=-3.0, vmax=3.0)
+#plt.pcolor(gridX_,gridY_,gridZ,cmap='jet', vmin=10.0, vmax=60.0)
+#
+#
+#plt.grid()
+#plt.xlim((-150.0,150.0))
+#plt.ylim((-150.0,150.0))
+#plt.show()
+time_index = ['{0}{1}'.format(str(x).zfill(2),str(y).zfill(2)) for x in range(24) for y in [0,30]]
+
+
+for x_ in range(43,91):
+    plt.figure()
+    plt.subplots(1,2)
+    plt.subplot(121)
+    real_ = np.ma.array(movie_points[:,:,x_,0], mask=movie_points[:,:,x_,0] == 0.)
+#    plt.pcolor(gridX_,gridY_,real_[:,:,x_,0].T,cmap='jet', vmin=0.0, vmax=1.0)
+    plt.pcolor(gridX_,gridY_,real_.T,cmap='RdGy', vmin=0.0, vmax=1.0)
+    plt.xlim((-150.0,150.0))
+    plt.ylim((-150.0,150.0))
+    plt.grid()
+    plt.subplot(122)
+    predicted_ = np.ma.array(movie_points[:,:,x_,1], mask=movie_points[:,:,x_,1] == 0.)
+#    plt.pcolor(gridX_,gridY_,predicted_[:,:,x_,1].T,cmap='jet', vmin=0.0, vmax=1.0)
+    plt.pcolor(gridX_,gridY_,predicted_.T,cmap='RdGy', vmin=0.0, vmax=1.0)
+    plt.xlim((-150.0,150.0))
+    plt.ylim((-150.0,150.0))
+    plt.grid()
+    
+    plt.suptitle('Actual and predicted fields for may 9th ' + time_index[x_ - 43] + ' UTC')
+    
+    plt.savefig('output/prediction_movies/Naive_Bayes/Plot_' + str(x_) + '.png')
+    
+
+
+'''
+
+        
         
 
 
